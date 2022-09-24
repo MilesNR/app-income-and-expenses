7452b050f30da93dc48d2863c59147188eeb2907
Nattakit​ Rattanakeha <nattakit.rattanakeha@gmail.com>
2022-09-24 16:01:38 +0700

    feat:add form & styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Transaction from "./Component/Transaction";
-import Items from "./Component/Items";
+import FormComponent from "./Component/FormComponent";
+
 function App() {
   const design = { color: "red", textAligh: "Center", fontsize: "1.5rem" };
   return (
     <div className="container">
-      <h2 style={design}>แอพบัญชีรายรับ - รายจ่าย</h2>
-      <Transaction />
+      <div className="app-size">
+        <h2 style={design}>แอพบัญชีรายรับ - รายจ่าย</h2>
+        <FormComponent />
+        <Transaction />
+      </div>
     </div>
   );
 }
---
+++ src/Component/FormComponent.js
@@ -0,0 +1,25 @@
+import "./FormComponent.css";
+
+const FormComponent = () => {
+  return (
+    <div>
+      <form>
+        <div className="form-control">
+          <label>ชื่อรายการ</label>
+          <input type="text" placeholder="ระบุชื่อรายการของคุณ"></input>
+        </div>
+        <div className="form-control">
+          <label>จำนวนเงิน</label>
+          <input type="number" placeholder="(+รายรับ , -รายจ่าย)"></input>
+        </div>
+        <div>
+          <button type="submit" className="btn">
+            เพิ่มข้อมูล
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormComponent;
