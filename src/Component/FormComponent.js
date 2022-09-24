f83c2f20d0fa10c98e569696c811bf37ffce0956
Nattakit​ Rattanakeha <nattakit.rattanakeha@gmail.com>
2022-09-24 16:21:04 +0700

    feat:add event

--- src/Component/FormComponent.js
+++ src/Component/FormComponent.js
@@ -1,16 +1,34 @@
 import "./FormComponent.css";
 
 const FormComponent = () => {
+  const inputTitle = (event) => {
+    console.log(event.target.value);
+  };
+  const inputAmount = (event) => {
+    console.log(event.target.value);
+  };
+  const saveItem = (event) => {
+    event.preventDefault();
+    console.log("บันทึกข้อมูลเรียบร้อย");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={saveItem}>
         <div className="form-control">
           <label>ชื่อรายการ</label>
-          <input type="text" placeholder="ระบุชื่อรายการของคุณ"></input>
+          <input
+            type="text"
+            placeholder="ระบุชื่อรายการของคุณ"
+            onChange={inputTitle}
+          ></input>
         </div>
         <div className="form-control">
           <label>จำนวนเงิน</label>
-          <input type="number" placeholder="(+รายรับ , -รายจ่าย)"></input>
+          <input
+            type="number"
+            placeholder="(+รายรับ , -รายจ่าย)"
+            onChange={inputAmount}
+          ></input>
         </div>
         <div>
           <button type="submit" className="btn">
