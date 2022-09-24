7a62b05c28b4e864acb6a8161d792a1b48432fa0
Nattakit​ Rattanakeha <nattakit.rattanakeha@gmail.com>
2022-09-24 23:18:39 +0700

    feat:add report section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import "./App.css";
 import Transaction from "./Component/Transaction";
 import FormComponent from "./Component/FormComponent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import DataContext from "./Data/DataContext";
+import ReportComponent from "./Component/ReportComponent";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const initState = [
+    { id: 1, title: "ค่าเช่าบ้าน", amount: -8000 },
+    { id: 2, title: "ค่าน้ำ", amount: -600 },
+    { id: 3, title: "เงินเดือน", amount: 80000 },
+  ];
+  const [items, setItems] = useState(initState);
+  const [reportIncome, setReportIncome] = useState(0);
+  const [reportExpense, setReportExpense] = useState(0);
   const onAddNewItem = (newItem) => {
     setItems((prevItem) => {
       return [newItem, ...prevItem];
     });
   };
+  useEffect(() => {
+    const amounts = items.map((items) => items.amount);
+    const income = amounts
+      .filter((element) => element > 0)
+      .reduce((total, element) => (total += element), 0);
+    const expense =
+      amounts
+        .filter((element) => element < 0)
+        .reduce((total, element) => (total += element), 0) * -1;
+    setReportIncome(income);
+    setReportExpense(expense);
+  }, [items, reportIncome, reportExpense]);
   return (
-    <div className="container">
-      <div className="app-size">
-        <h2 className="header-design">แอพบัญชีรายรับ - รายจ่าย</h2>
-        <FormComponent onAddItem={onAddNewItem} />
-        <Transaction items={items} />
+    <DataContext.Provider
+      value={{
+        income: reportIncome,
+        expense: reportExpense,
+      }}
+    >
+      <div className="container">
+        <div className="app-size">
+          <h2 className="header-design">แอพบัญชีรายรับ - รายจ่าย</h2>
+          <ReportComponent />
+          <FormComponent onAddItem={onAddNewItem} />
+          <Transaction items={items} />
+        </div>
       </div>
-    </div>
+    </DataContext.Provider>
   );
 }
 
--- src/Component/Transaction.js
+++ src/Component/Transaction.js
@@ -1,15 +1,19 @@
+import { useContext } from "react";
 import Items from "./Items";
 import "./Transaction.css";
+import DataContext from "../Data/DataContext";
 
 let Transaction = (props) => {
   const { items } = props;
-
+  const name = useContext(DataContext);
   return (
-    <ul className="item-list">
-      {items.map((element) => {
-        return <Items {...element} key={element.id} />;
-      })}
-    </ul>
+    <>
+      <ul className="item-list">
+        {items.map((element) => {
+          return <Items {...element} key={element.id} />;
+        })}
+      </ul>
+    </>
   );
 };
 
